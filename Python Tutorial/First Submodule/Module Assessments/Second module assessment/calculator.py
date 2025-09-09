618dcee232e3c6efbf4065d1d871a5de1664744c
Dirkthecat <vsarkany@uwo.ca>
2025-09-09 08:47:29 -0400

    Add files via upload

---
+++ Python Tutorial/First Submodule/Module Assessments/Second module assessment/calculator.py
@@ -0,0 +1,11 @@
+first_number = float(input("Enter first number: "))
+second_number = float(input("Enter second number: "))
+a = first_number + second_number
+b = first_number - second_number
+c = first_number * second_number
+d = first_number / second_number
+print(f"The sum is {a}.\n", f"The difference is {b}.\n",f"The product is {c}.\n", f"First value divided by second value is {d}.")
+
+
+
+
